f9378c2e011e16f07e36b6ebec3ccbbc1664dd6b
Eric <ericabwa90@gmail.com>
2025-11-22 15:51:38 +0100

    fit: none specific ;

--- working_with_stirngs.py
+++ working_with_stirngs.py
@@ -4,4 +4,16 @@ first_name = "ngwa"
 last_name ="jude"
 print (first_name.upper()) # turn to uppercase 
 print ( last_name.title()) # adds first letter as title
+food = "RICE"
+print(food.lower())
+print (food[2])
+count = len(food)
+print(count)
+#long ahh string
+languages ="\n\nPython\nC\nJava\nC++"
+print(languages)
 
+#char
+character = 'j'
+if character in last_name:
+    print(f"the letter {character} exist in {last_name}")
