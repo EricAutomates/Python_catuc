3421f2cf2dd8b6192c23dcacc8a6ec34e79d65df
Eric <ericabwa90@gmail.com>
2026-01-01 21:17:11 +0100

    fix: still forgot tosave 1 file. im tired

---
+++ working_with_conditionals.py
@@ -0,0 +1,34 @@
+
+age = 12
+if (age < 4 ):
+    print ("$25")
+
+if(age < 15):
+    print ("$25")
+  
+if(age < 65):
+    print ("$65")
+
+else: 
+    print("$20")
+
+
+num = 2
+if (num < 1 or num == 2):
+    print ("number is even")
+
+
+user = "John"
+has_enough_money = True 
+is_always_punctual = True
+
+if (has_enough_money == True and is_always_punctual == True):
+    print ("we can borrow money to",user )
+
+
+food = ["rice", "eru", "beans"]
+if "rice" in food:
+    print("do not add more rice")
+else: 
+    ("add more rice")
+
