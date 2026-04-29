33bcd5b5abaec364739bf926ffe733565393c2d2
Eric <ericabwa90@gmail.com>
2026-04-29 15:43:33 +0100

    initial commit

---
+++ ca.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void drawDiamond(int size)
+{
+    // upper part
+    for (int i = 1; i <= size; i++)
+    {
+        for (int s = 0; s < size - i; s++)
+            cout << " ";
+
+        for (int j = 0; j < (2 * i - 1); j++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    // lower part
+    for (int i = size - 1; i >= 1; i--)
+    {
+        for (int s = 0; s < size - i; s++)
+            cout << " ";
+
+        for (int j = 0; j < (2 * i - 1); j++)
+            cout << "*";
+
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int size;
+
+    cout << "enter size: ";
+    cin >> size;
+
+    drawDiamond(size);
+
+    return 0;
+}
